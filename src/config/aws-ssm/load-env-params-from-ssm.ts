775b1675266eb70c79736248b950e71a0e14660d
ariel-bar <ariel.bar@zoominfo.com>
2024-12-02 00:53:13 +0200

    feat: add logs

--- src/config/aws-ssm/load-env-params-from-ssm.ts
+++ src/config/aws-ssm/load-env-params-from-ssm.ts
@@ -1,18 +1,14 @@
 import { SSM } from 'aws-sdk';
 
 const appName = 'mehubarim-server';
-const keys = [
-  'jwt-expiration',
-  'jwt-secret',
-  'mmongodb-uri',
-  'port',
-];
+const keys = ['jwt-expiration', 'jwt-secret', 'mmongodb-uri', 'port'];
 
 async function getParameter(
   ssm: SSM,
   name: string,
 ): Promise<string | undefined> {
   try {
+    console.info(`before getParameter ${name}`);
     const data = await ssm
       .getParameter({
         Name: name,
@@ -20,6 +16,7 @@ async function getParameter(
       })
       .promise();
 
+    console.info(`after getParameter ${data}`);
     return data.Parameter?.Value;
   } catch (error) {
     console.error('Error retrieving parameter:', error);
@@ -31,11 +28,11 @@ export async function loadEnvParamsFromSsm() {
   if (process.env.NODE_ENV === 'local') {
     return;
   }
-  console.info('loading env params from SSM');
+  console.info(`loading env params from SSM env:${process.env.NODE_ENV}`);
   const ssm = new SSM();
   await keys.forEach(async (key) => {
     const value = await getParameter(ssm, `/${appName}/${process.env}/${key}`);
-    console.info(`/${appName}/${process.env}/${key}`,value);
+    console.info(`/${appName}/${process.env}/${key}`, value);
     process.env[key.replace('-', '_').toUpperCase()] = value;
   });
 }
